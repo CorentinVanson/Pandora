58b21c8292da027e69a180631f5bfdd31b50a510
Alban <alban.delcourt@isen-lille.fr>
2016-02-20 23:24:19 -0500

    minor changes to add mongodb in the node server

--- app.js
+++ app.js
@@ -5,8 +5,12 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/animaldb');
+
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var users = require('./routes/about');
 var getRandomMap = require('./routes/map/getRandomMap');
 
 var app = express();
@@ -22,6 +26,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+// Make our db accessible to our router
+app.use(function(req,res,next){
+  req.db = db;
+  next();
+});
 
 app.use('/', routes);
 app.use('/users', users);
